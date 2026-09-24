33f1fe951cb68f86d26c702ae9c3e8f90c1c6e44
unknown <782468678@qq.com>
2026-09-24 16:14:47 +0800

    fix: apply configured MQTT broker WebSocket URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mqtt_integration",
-    version="0.1.4",
+    version="0.1.5",
     description="ERPNext Desk entry point for the Starchart MQTT web console",
     author="Skychip",
     packages=find_packages(),
