7c09e79d3a92861c248b4bb1d70dce9a14308439
unknown <782468678@qq.com>
2026-09-24 14:43:55 +0800

    feat: sync latest MQTT web client

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mqtt_integration",
-    version="0.1.3",
+    version="0.1.4",
     description="ERPNext Desk entry point for the Starchart MQTT web console",
     author="Skychip",
     packages=find_packages(),
